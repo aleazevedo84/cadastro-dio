a248605d7c6f9dd120f88f77edc8350147141beb
Alessandro Azevedo <aleazevedo84@outlook.com>
2022-12-08 21:43:49 -0300

    Criação de tela de cadastro

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -8,7 +8,11 @@ export const ButtonContainer = styled.button`
     color: #FFFFFF;
     padding: 2px 12px;
     min-width: 120px;
-    width: 100%;
+    width: 100%;    
+    cursor: pointer;
+    &:hover {
+        background: #E4105D;
+    }
     
     ${({variant}) => variant !== "primary" && css`
         min-width: 167px;
